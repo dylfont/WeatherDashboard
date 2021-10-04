13bd0234b0bc0b2a13cbfa6427a8266036dc4ad4
Dylan <dylanfontenot@protonmail.com>
2021-10-03 19:33:09 -0500

    search and save functions

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var APIkey="9510ac21d4a2f3da6c213cec7857f623"
 var APIurl="https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
 var APIurl2= "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
-
+var searchInput= document.getElementById("searchInput")
 
 function saveCity(){
-  console.log("hello") 
+ var input= searchInput.value   
+  console.log(input) 
+
 }
 
 const searchBTN= document.getElementById("searchBTN")
